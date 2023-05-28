9a840aa615d1ef61713c580801a5d4f29a964a53
joel <joel_zerg_pro@outlook.com>
2023-05-27 23:50:02 -0500

    sdk_used_connect_and_close_print_finished

--- app/src/main/java/com/example/appconsettur/MainActivity.kt
+++ app/src/main/java/com/example/appconsettur/MainActivity.kt
@@ -10,13 +10,19 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.view.WindowCompat
+import com.bxl.config.editor.BXLConfigLoader
 import jpos.POSPrinter
 import jpos.POSPrinterConst
+import jpos.config.JposEntry
 import java.util.UUID
 
 const val REQUEST_ENABLE_BT = 1
@@ -25,8 +31,14 @@ class MainActivity : AppCompatActivity() {
     lateinit var bluetoothAdapter: BluetoothAdapter
     var mAddressDevices: ArrayAdapter<String>? = null
     var mNameDevices: ArrayAdapter<String>? = null
+    private lateinit var posPrinter: POSPrinter
+    var productName = BXLConfigLoader.PRODUCT_NAME_SPP_R200III
+    var bxlConfigLoader: BXLConfigLoader? = null
+    val ESCAPE_CHARACTERS = String(byteArrayOf(0x1b, 0x7c))
 
-    var posPrinter:POSPrinter?=null
+
+
+//    var posPrinter:POSPrinter?=null
 
 
 
@@ -43,7 +55,6 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        posPrinter = POSPrinter(this)
 
 
         mAddressDevices = ArrayAdapter(this, android.R.layout.simple_list_item_1)
@@ -55,6 +66,7 @@ class MainActivity : AppCompatActivity() {
         var sendText = findViewById<Button>(R.id.sendText)
         var idSpinDisp = findViewById<Spinner>(R.id.spinner)
         var idTextOut = findViewById<EditText>(R.id.editTextText)
+        var closePrint=findViewById<Button>(R.id.closePrint)
 
         var someActivityResultLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
@@ -121,18 +133,44 @@ class MainActivity : AppCompatActivity() {
             }
         }
         connect.setOnClickListener {
+            productName = BXLConfigLoader.PRODUCT_NAME_SPP_R200III
+            bxlConfigLoader= BXLConfigLoader(this)
+            bxlConfigLoader!!.openFile()
             try {
-                if (m_bluetoothSocket == null || !m_isConnected) {
-                    val IntValSpin = idSpinDisp.selectedItemPosition
-                    m_address = mAddressDevices!!.getItem(IntValSpin).toString()
-                    Toast.makeText(this, "Conectando a $m_address", Toast.LENGTH_SHORT).show()
-                    bluetoothAdapter?.cancelDiscovery()
-                    val device: BluetoothDevice = bluetoothAdapter.getRemoteDevice(m_address)
-                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUid)
-                    m_bluetoothSocket!!.connect()
+//                if (m_bluetoothSocket == null || !m_isConnected) {
+//                    val IntValSpin = idSpinDisp.selectedItemPosition
+//                    m_address = mAddressDevices!!.getItem(IntValSpin).toString()
+//                    Toast.makeText(this, "Conectando a $m_address", Toast.LENGTH_SHORT).show()
+//                    bluetoothAdapter?.cancelDiscovery()
+//                    val device: BluetoothDevice = bluetoothAdapter.getRemoteDevice(m_address)
+//                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUid)
+//                    m_bluetoothSocket!!.connect()
+//                }
+//                Toast.makeText(this, "Conectado a $m_address", Toast.LENGTH_SHORT).show()
+//                Log.i("MainActivity", "Conectado a $m_address")
+                for (entry in bxlConfigLoader?.entries!!) {
+                    val config = entry as JposEntry
+                    val logicalNames = config.logicalName
+                    Log.i("MainActivity", logicalNames)
+
+                    bxlConfigLoader!!.removeEntry(logicalNames)
                 }
-                Toast.makeText(this, "Conectado a $m_address", Toast.LENGTH_SHORT).show()
-                Log.i("MainActivity", "Conectado a $m_address")
+
+                var logicalName="SPP-R200III_110362"
+                bxlConfigLoader!!.addEntry(logicalName,
+                    BXLConfigLoader.DEVICE_CATEGORY_POS_PRINTER,
+                    productName,
+                    BXLConfigLoader.DEVICE_BUS_BLUETOOTH,
+                    "74:F0:7D:A8:85:25")
+                bxlConfigLoader!!.saveFile()
+
+                posPrinter = POSPrinter(this)
+
+                posPrinter.open("SPP-R200III_110362")
+                posPrinter.claim(5000*2)
+                posPrinter.deviceEnabled = true
+                Toast.makeText(this, "successful connect", Toast.LENGTH_SHORT).show()
+
             } catch (e: Exception) {
                 e.printStackTrace()
                 Toast.makeText(this, "Error al conectar a $m_address", Toast.LENGTH_SHORT).show()
@@ -142,26 +180,73 @@ class MainActivity : AppCompatActivity() {
         //"|N|lA|aM|1hC|1vCBixolon Text Print!!
 //        posPrinter!!.printNormal(POSPrinterConst.PTR_S_RECEIPT, strOption + data)
 
-//        sendText.setOnClickListener {
-////            posPrinter?.printNormal(POSPrinterConst.PTR_S_RECEIPT, "Hello World")
-//
-//
-//                val msg: String = "Bixolon Text Print!!"
-//                try {
-////                    m_bluetoothSocket!!.outputStream.write(msg.toByteArray())
-////                    Toast.makeText(this, "Mensaje enviado", Toast.LENGTH_SHORT).show()
-////                    Log.i("MainActivity", "Mensaje enviado")
-//                    posPrinter?.printNormal(POSPrinterConst.PTR_S_RECEIPT, "|N|lA|aM|1hC|1vC" + msg)
-//
-//                } catch (e: Exception) {
-//                    e.printStackTrace()
-//                    Toast.makeText(this, "Error al enviar mensaje", Toast.LENGTH_SHORT).show()
-//                    Log.i("MainActivity", "Error al enviar mensaje")
-//                }
+        closePrint.setOnClickListener{
+            try {
+                posPrinter.close()
+                Toast.makeText(this, "Close Print", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "Error al cerrar a $m_address", Toast.LENGTH_SHORT).show()
+                Log.i("MainActivity", "Error al cerrar a $m_address")
+            }
+        }
+        sendText.setOnClickListener {
+//            posPrinter?.printNormal(POSPrinterConst.PTR_S_RECEIPT, "Hello World")
+
+//            try{
+//                bxlConfigLoader= BXLConfigLoader(this)
+//                bxlConfigLoader!!.openFile()
+//            } catch (e:Exception){
+//                e.printStackTrace()
+//                Log.i("MainActivity", "Error al conectar")
 //            }
+                val msg: String = "SMART\n"
+                try {
+                    posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT,(ESCAPE_CHARACTERS + "uC"+ESCAPE_CHARACTERS + "cA")+ msg)
+                    posPrinter.printNormal(POSPrinterConst.PTR_S_RECEIPT,(ESCAPE_CHARACTERS + "uC"+ESCAPE_CHARACTERS + "cA")+ "NEW LINE\n")
+//                    for (entry in bxlConfigLoader?.entries!!) {
+//                        val config = entry as JposEntry
+//                        val logicalNames = config.logicalName
+//                        Log.i("MainActivity", logicalNames)
 //
+//                        bxlConfigLoader!!.removeEntry(logicalNames)
+//                    }
+//                    val ESCAPE_CHARACTERS = String(byteArrayOf(0x1b, 0x7c))
 //
-//        }
+//                    var logicalName="SPP-R200III_110362"
+//                    bxlConfigLoader!!.addEntry(logicalName,
+//                        BXLConfigLoader.DEVICE_CATEGORY_POS_PRINTER,
+//                        productName,
+//                        BXLConfigLoader.DEVICE_BUS_BLUETOOTH,
+//                        "74:F0:7D:A8:85:25")
+//                    bxlConfigLoader!!.saveFile()
+//
+//                    posPrinter = POSPrinter(this)
+//
+//                    posPrinter.open("SPP-R200III_110362")
+//                    posPrinter.claim(5000*2)
+//                    posPrinter.deviceEnabled = true
 
 
-    }}
+
+
+
+//                    posPrinter.close()
+//                    m_bluetoothSocket!!.outputStream.write(msg.toByteArray())
+//                    Toast.makeText(this, "Mensaje enviado", Toast.LENGTH_SHORT).show()
+                    Log.i("MainActivity", "Mensaje enviado")
+//                    posPrinter?.printNormal(POSPrinterConst.PTR_S_RECEIPT, "|N|lA|aM|1hC|1vC" + msg)
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    Toast.makeText(this, "Error al enviar mensaje", Toast.LENGTH_SHORT).show()
+                    Log.i("MainActivity", "Error al enviar mensaje")
+                }
+            }
+
+
+        }
+
+
+
+}
